50915385d3db5ee85415733047c1eef4a930e3af
laila <laila@fake.com>
2018-03-05 23:12:55 +0200

    added HashMap to have the cd as a key and the CD object as a value

--- src/main/java/CreateJson.java
+++ src/main/java/CreateJson.java
@@ -1,22 +1,16 @@
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 
-/**
- * Created by Liuda on 3/4/2018.
- */
+
 public class CreateJson {
 
     public static void main(String[] args){
-            //throws JsonMappingException{
-
-
 
         CD cdNirvana = new CD("d1","d2","d3","d4","d5","d6","d7");
         CD cdCranberries = new CD("a1","a2","a3","a4","a5","a6","a7");
@@ -26,11 +20,12 @@ public class CreateJson {
         Catalog music  = new Catalog(my_list);
 
         ObjectMapper mapper = new ObjectMapper();
-      //  ObjectNode objectNode1 = mapper.createObjectNode();
-
+        Map<String,CD> myMap = new HashMap<String,CD>();
+        myMap.put("Cd1",cdNirvana);
+        myMap.put("Cd2",cdCranberries);
 
         try {
-            mapper.writeValue(new File("music.json"), cdNirvana);
+            mapper.writeValue(new File("music.json"), myMap);
         }catch (JsonMappingException e) {
             e.printStackTrace();
         } catch (IOException e) {
